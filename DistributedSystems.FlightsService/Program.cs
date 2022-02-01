8dd7fb063159064b0e84d4ccb22f6719aa429784
Mahdi Koohestani <koohestani@atighgasht.ir>
2022-02-01 23:23:14 +0330

    add razor

--- DistributedSystems.FlightsService/Program.cs
+++ DistributedSystems.FlightsService/Program.cs
@@ -12,18 +12,26 @@ namespace DistributedSystems.FlightsService
 
         public static void Main(string[] args)
         {
-            Server server = new Server
+            try
             {
-                Services = { Flight.Flights.BindService(new FlightProtoImpl()) },
-                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
-            };
-            server.Start();
+                Server server = new Server
+                {
+                    Services = { Flight.Flights.BindService(new FlightProtoImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
 
-            Console.WriteLine("Greeter server listening on port " + Port);
-            Console.WriteLine("Press any key to stop the server...");
-            Console.ReadKey();
+                Console.WriteLine("Greeter server listening on port " + Port);
+                Console.WriteLine("Press any key to stop the server...");
+                Console.ReadKey();
 
-            server.ShutdownAsync().Wait();
+                server.ShutdownAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
---
+++ DistributedSystems.Web/Data/grpcServices.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace DistributedSystems.Web.Data
+{
+    public class grpcServices
+    {
+        public async Task<Flight.GetAirPortsResponse> GetAirportsAsync()
+        {
+            try
+            {
+                Channel channel = new Channel("127.0.0.1:30051", ChannelCredentials.Insecure);
+                var client = new Flight.Flights.FlightsClient(channel);
+
+                var airports = await client.GetAirportsListAsync(new Flight.GetAirPortsRequest { International = false, Title = "" });
+                return airports;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return null;
+        }
+    }
+}
